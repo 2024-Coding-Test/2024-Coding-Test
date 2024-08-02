358d8b31faafb7962580b587e465e9b80e32b8dc
Yerinnnn <shinyerin0216@gmail.com>
2024-08-03 01:32:44 +0900

    [week1-2] solve 1138

--- ye-rin/8979.py
+++ ye-rin/8979.py
@@ -7,7 +7,7 @@ def calculate_rank(countries, k):
         # 첫 번째 국가(i=0)는 이전 국가가 없으므로, i > 0으로 첫 번째 국가를 건너뜀
         # 현재 국가와 이전 국가의 메달 정보가 같지 않다면, 다른 등수 부여
         if i > 0 and country[1:] != sorted_countries[i-1][1:]:
-            current_rank = i + 1
+            current_rank = i + 1    # 인덱스 i에 1을 더하므로서, 같은 메달 수의 국가들 다음에 오는 국가는 그 국가들의 수를 건너뛴 등수를 받음(공동 등수 채택)
         ranks[country[0]] = current_rank    # ex) ranks[1번 국가] = 3등
     
     return ranks[k]
