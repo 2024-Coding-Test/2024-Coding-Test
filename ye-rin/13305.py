3b28a085f0461f961d025c12be477ab6b0420e40
Yerinnnn <shinyerin0216@gmail.com>
2024-08-04 17:44:23 +0900

    [week1-4] solve 13305

---
+++ ye-rin/13305.py
@@ -0,0 +1,14 @@
+N = int(input())
+distance = list(map(int, input().split()))
+price = list(map(int, input().split()))
+
+minPrice = price[0]
+total = 0
+
+for i in range(N-1):
+    if minPrice > price[i]:
+        minPrice = price[i]
+
+    total += (minPrice*distance[i])
+
+print(total)
