2e7f2970cb89fa7c9127cfebc95730dc01c68fb4
Kite_Z <duswl339@ajou.ac.kr>
2024-08-13 05:21:08 +0900

    [week2-3] solve 19941

--- yeon-z/19941.py
+++ yeon-z/19941.py
@@ -1,28 +1,19 @@
 # 햄버거 분배
-# 1. 자주 나오는 단어일수록 앞에 배치
-# 2. 해당 단어의 길이가 갈수록 앞에 배치한다.
-# 3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다.
 
 from sys import stdin
 
-N, M = map(int, stdin.readline().strip().split())
-alphabet = {}
-dict = []
+# K 먹을 수 있는 거리
+N, K = map(int, stdin.readline().strip().split())
 
-for _ in range(N):
-    alpha = stdin.readline().strip()
-    if len(alpha) >= M:
-        if alpha in alphabet:
-            alphabet[alpha] += 1
-        else:
-            alphabet[alpha] = 1
+tables = list(map(str, stdin.readline().strip()))
+count = 0
 
-for a in alphabet:
-    dict.append([a, alphabet[a]])
+for i in range(len(tables)):
+    if tables[i] == 'P':
+        for j in range(max(i-K, 0), min(i+K+1, N)):
+            if tables[j] == 'H':
+                tables[j] = 'X'
+                count += 1
+                break
 
-# print(N, M)
-
-dict.sort(key=lambda x: [-x[1], -len(x[0]), x[0]])
-for d in dict:
-    print(d[0])
-# print(dict)
+print(count)
