f04f61e6e1116e9f9138ea808ce3433915070205
Kite_Z <duswl339@ajou.ac.kr>
2024-08-27 13:34:30 +0900

    [week5-1] solve 42586

--- yeon-z/programmers/algorithm-kit/stack-queue/42586.py
+++ yeon-z/programmers/algorithm-kit/stack-queue/42586.py
@@ -1,4 +1,5 @@
 # 기능개발
+# 현재코드
 def solution(progresses, speeds):
     answer = []
     day = 0
@@ -20,4 +21,29 @@ def solution(progresses, speeds):
         if i == len(progresses) - 1:
             answer.append(cnt)
 
+    return answer
+
+
+# 옛날 코드
+import math
+
+
+def solution(progresses, speeds):
+    answer = []
+    diff = []
+
+    for idx in range(len(progresses)):
+        diff.append(math.ceil((100 - progresses[idx]) / speeds[idx]))
+
+    t = diff[0]
+    cnt = 1
+    for idx in range(1, len(diff)):
+        if diff[idx] <= t:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            t = diff[idx]
+            cnt = 1
+
+    answer.append(cnt)
     return answer
